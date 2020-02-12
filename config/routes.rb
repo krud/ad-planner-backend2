c0c5ddc43cb9ca31b90ae140ff98593b852f77f7
KellyRudnicki <kelly.rudnicki@comcast.net>
2020-02-12 16:38:52 -0700

    user/room controllers, models, and routes created as well as login capability

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :rooms
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+
+  post "login", to: "authentication#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
